3e14cfa688ec3b08814804a5f5fcf6ddadb0a40a
jaymie <jaymie9019@gmail.com>
2022-06-29 09:21:21 +0800

    feat modify go.work

---
+++ examples/yaml/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Student struct {
+	Name    string    `yaml:"name"`
+	Age     int       `yaml:"age"`
+	Teacher Teacher   `yaml:"teacher"`
+	Friends []Student `yaml:"friends"`
+}
+
+type Teacher struct {
+	Name   string   `yaml:"name"`
+	Course []string `yaml:"course"`
+}
+
+var studentYaml = `
+name: lajidai
+age: 30
+teacher:
+  name: wanglaoshi
+  course: 
+    - Math
+    - English
+friends:
+  - name: xiaowang
+    age: 20
+  - name: xiaohong
+    age: 28
+`
+
+func main() {
+	student := Student{}
+	//_ := yaml.UnmarshalFromString(studentYaml, &student)
+	fmt.Println(student)
+
+}
